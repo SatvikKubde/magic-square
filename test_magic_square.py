f9f8882c83b82edb0149b3a9dd2ada7c289708d5
Satvik Kubde <satvikkubde@gmail.com>
2025-08-31 11:31:27 +0530

    MAGIC-2: add unit tests for solver

--- test_magic_square.py
+++ test_magic_square.py
@@ -1,22 +1,24 @@
-# test_magic_square.py
 import unittest
 from magic_square import solve
 
 class TestMagicSquare(unittest.TestCase):
-    def test_solution(self):
+    def test_solution_sum(self):
         grid, s = solve()
-        # magic sum should be 18
+        # check magic sum
         self.assertEqual(s, 18)
 
-        # check rows
+    def test_rows(self):
+        grid, s = solve()
         for row in grid:
             self.assertEqual(sum(row), 18)
 
-        # check columns
+    def test_columns(self):
+        grid, s = solve()
         for c in range(3):
             self.assertEqual(sum(grid[r][c] for r in range(3)), 18)
 
-        # check diagonals
+    def test_diagonals(self):
+        grid, s = solve()
         self.assertEqual(grid[0][0] + grid[1][1] + grid[2][2], 18)
         self.assertEqual(grid[0][2] + grid[1][1] + grid[2][0], 18)
 
